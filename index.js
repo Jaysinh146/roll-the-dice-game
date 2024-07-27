b50c93754ca4075c6cfa04ecea9af88afa8ecb3a
Jaysinh Abasaheb Patankar <143993843+Jaysinh146@users.noreply.github.com>
2024-07-27 21:41:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function rollDice() {
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;  // 1-6
+
+    var randomDiceImage = "dice" + randomNumber1 + ".png";   // dice1.png - dice6.png
+
+    var randomImageSource = "images/" + randomDiceImage;
+
+    var Image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    var randomeDiceImage2 = "images/" + "dice" + randomNumber2 + ".png"; 
+
+    var Image2 = document.querySelectorAll("img")[1].setAttribute("src", randomeDiceImage2);
+
+    //if player 1 wins -
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 wins !!";
+    } 
+    else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "PLayer 2 wins !!";
+    } else {
+        document.querySelector("h1").innerHTML = "Draw !!";
+    }
+
+}
